5c1afe90871ae5d05e0215be2968742e5e3d7ae5
Zaya0454 <nik249096@gmail.com>
2023-08-18 00:07:04 +0500

    Takrorlash

---
+++ Online.py
@@ -0,0 +1,33 @@
+#print("Minutlarni kiriting:")  //START
+#a=int(input())
+#print(a, "min - bu teng bo'ladi", a//60, "soat va ", a%60, "minutga")  //END
+
+#print("Yoshingizni kiriting: ")   //START
+#a = int(input())
+#print("Kirish mumkin:") if a >= 18 else print("Kirish mumkin EMAS!!!")   //END
+
+#print("Parolni kiriting:  ")       //START
+#a = (input())
+#print("Parolni qaytaring: ")
+#b = (input())
+#print("Parol o'rnatildi!") if a == b else print("Parolingiz qabul qilinmadi!!!")  //END
+
+#print("Qidirilayotgan sonni yozing:  ")          //START
+#a = int(input())
+#print("Qaysi sondan qidirish boshlansin?   ")
+#b = int(input())
+##print("Qaysi songacha qidirilsin?   ")
+#c = int(input())
+#if a > b and a < c:
+#    print("Kiradi!")
+#else:
+#    print("Kirmaydi!!!")
+
+ # ////////////////////////  YOKI   //////////////
+
+#print("Sonni kiriting:  ")
+#a = int(input())
+#if a > -1 and a < 1000:
+#    print("Kiradi!")
+#else:
+#    print('Kirmaydi!!!')                          //END
