315d716a90a50db31c807f07f6f45a0070ea1bd7
Zaya0454 <nik249096@gmail.com>
2023-09-02 23:23:26 +0500

    xatolar

---
+++ Xatolar.py
@@ -0,0 +1,8 @@
+#xatolar bo'yicha misollar
+
+#try:
+#    a = int(input("a sonni kiriting: "))
+#    b = int(input("b sonni kiriting: "))
+#    print( a / b)
+#except ZeroDivisionError:
+#    print("Siz kiritgan sonlardan bittasi 0 ga teng! Iltimos boshqa son kiriting!!!")
