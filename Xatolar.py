2ea07187b5456780f91afc28c915aa754ecd1a08
Zaya0454 <nik249096@gmail.com>
2023-09-03 22:23:21 +0500

    xatolar

--- Xatolar.py
+++ Xatolar.py
@@ -6,3 +6,12 @@
 #    print( a / b)
 #except ZeroDivisionError:
 #    print("Siz kiritgan sonlardan bittasi 0 ga teng! Iltimos boshqa son kiriting!!!")
+
+#try:
+#    a = int(input("a sonni kiriting: "))
+#    b = int(input("b sonni kiriting: "))
+#    print(a / b)
+#except ZeroDivisionError:
+#    print("Sonlardan bittasiga 0 kiritgansiz!")
+#except ValueError:
+#    print("Siz son kiritishingiz kerak!")
