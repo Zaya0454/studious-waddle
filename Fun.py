ac0a4d36773ce471f972bbd56412d19aba6e3d0d
Zaya0454 <nik249096@gmail.com>
2023-09-04 23:28:41 +0500

    Tekshiruv

---
+++ Fun.py
@@ -0,0 +1,13 @@
+input("Hi  (Click to ENTER!)")
+print("Hello idiot!")
+
+input("Hmm  (Enter!)")
+print("Hug me more")
+
+input("Mm  (Enter!)")
+print("Mary me")
+
+input("LOL!  (Enter)")
+print("Lots of love")
+
+print("BU hammasi xazil!!!!!!")
